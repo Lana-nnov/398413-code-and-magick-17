00b3e1f8ad048df01dd2df6c931b3d74def09370
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-06-07 10:21:39 +0300

    Внесены изменения в учебный проект Холст

--- js/stat.js
+++ js/stat.js
@@ -19,6 +19,13 @@ function randomInteger(min, max) {
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var TEXT_WIDTH = 20;
+var BAR_WIDTH = 40;
+var BAR_MAXHEIGHT = 150;
+var GAP_COLUMN = 50;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -26,24 +33,27 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 130, 40);
-  ctx.fillText('Список результатов:', 130, 60);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP + GAP, CLOUD_Y + GAP + GAP + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP + GAP, CLOUD_Y + GAP + GAP + GAP + TEXT_WIDTH);
 
-  var stepX = 50;
+  var stepX = BAR_WIDTH + GAP;
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    stepX += 90;
-    ctx.fillStyle = 'hsl(240,' + randomInteger(1, 100) + '%,' + 50 + '%)';
+    stepX += BAR_WIDTH + GAP_COLUMN;
+    ctx.fillStyle = '#000';
+    var BAR_HEIGHT = BAR_MAXHEIGHT * Math.round(times[i]) / maxTime;
+    ctx.fillText(Math.round(times[i]), stepX, CLOUD_HEIGHT - TEXT_WIDTH - BAR_HEIGHT - GAP);
+    ctx.fillStyle = 'hsl(240,' + randomInteger(1, 100) + '%, 50%)';
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect(stepX, 240 - (150 * Math.round(times[i])) / maxTime, 40, (150 * Math.round(times[i])) / maxTime);
+    ctx.fillRect(stepX, CLOUD_HEIGHT - TEXT_WIDTH - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], stepX, 260);
+    ctx.fillText(names[i], stepX, CLOUD_HEIGHT);
   }
 };

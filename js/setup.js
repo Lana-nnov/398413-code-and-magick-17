12fcdfc90fd37e400520e4a46c762b4625762231
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-08-11 21:42:32 +0300

    Добавлены волшебники

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,8 @@
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var number = 4;
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var number = 4;
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');

f80c9e8688e69e31dbc9209343dd010f15de5223
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-08-11 20:43:04 +0300

    добавлены еще 4 волшебника

---
+++ js/setup.js
@@ -0,0 +1,52 @@
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var number = 4;
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+// находим контейнер с фото - тег <template> с классом pictures и внутри ccылку с id picture, в которой одно изображение
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
+var generateWizardData = function (count) {
+  var wizards = [];
+  for (var i = 0; i < count; i++) {
+    var wizard = {
+      name: names[Math.floor(Math.random() * names.length)] + ' ' + surnames[Math.floor(Math.random() * surnames.length)],
+      coatColor: coatColors[Math.floor(Math.random() * coatColors.length)],
+      eyesColor: eyesColors[Math.floor(Math.random() * eyesColors.length)]
+    };
+    wizards[i] = wizard;
+  };
+
+  return wizards;
+};
+
+var renderWizards = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = array[i].name;
+    wizardElement.querySelector('.wizard-coat').style.fill = array[i].coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = array[i].eyesColor;
+
+    fragment.appendChild(wizardElement);
+  }
+  similarListElement.appendChild(fragment);
+};
+
+
+var wizards = generateWizardData(number);
+renderWizards(wizards);

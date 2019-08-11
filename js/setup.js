efe2b9cdc96332a5d389ce76502d32a4286b69fc
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-08-11 22:00:40 +0300

    внесены изменения после проверки линтеров

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var names = ['Иван', 'Хуан Себастьян', 'Мария2', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -10,10 +12,6 @@ userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 // находим контейнер с фото - тег <template> с классом pictures и внутри ccылку с id picture, в которой одно изображение
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -30,7 +28,7 @@ var generateWizardData = function (count) {
       eyesColor: eyesColors[Math.floor(Math.random() * eyesColors.length)]
     };
     wizards[i] = wizard;
-  };
+  }
 
   return wizards;
 };
